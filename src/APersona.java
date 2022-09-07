42c24ae1fdb280e03e64755a6635bb1c74a2c3d2
NewAngie <102178971+NewAngie@users.noreply.github.com>
2022-09-06 20:18:27 -0500

    Initial commit

---
+++ src/APersona.java
@@ -0,0 +1,7 @@
+public class APersona{
+    public static void main(String[] args) {
+        Persona humano1=new Persona();
+        System.out.println("Tu edad es: "+ humano1.edad);
+    }
+}
+
---
+++ src/Persona.java
@@ -0,0 +1,8 @@
+public class Persona {
+    String nombre;
+    int edad=15;
+    String genero;
+
+    public Persona(){
+    }
+}
